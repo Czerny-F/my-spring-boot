644a02f737a3e7da373dc874eaf125777d2abcad
Czerny-F <dummy00.cz@gmail.com>
2018-11-21 20:22:30 +0900

    web security

--- src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -1,23 +1,29 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.UserService;
 import com.example.demo.entities.User;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
-  @Autowired
-  private UserRepository userRepository;
 
-    @GetMapping("/users")
-    public @ResponseBody Iterable<User> userList() {
-        return userRepository.findAll();
-    }
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/users")
+  public Iterable<User> userList() {
+    return userService.selectAll();
+  }
+
+  @PostMapping("/register")
+  public User register(@RequestParam String username, @RequestParam String password) {
+    return userService.registerUser(username, password);
+  }
 }
